e883035d71c5f69c8da33e4d40aa50f626d50495
Anjali P <panjalianju555@gmail.com>
2024-10-12 12:18:50 +0400

    First commit

---
+++ form-handler.php
@@ -0,0 +1,19 @@
+<?php
+$name = $_POST['name'];
+$vistor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message =  $_POST['message'];
+
+$email_from =  'info@yourwebsite.com';
+$email_subject= 'New form submission';
+$email_body = "User Name: $name .\n".
+              "User Email: $vistor_email.\n".
+              "Subject:$subject.\n".
+              "User Message: $message .\n";
+$to = "panjalianju555@gmail.com";
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $vistor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location:contact.html")
+
+?>
